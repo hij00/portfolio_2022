3b5de7baa5023295da114631f8650676f7cec50d
hijeong_ <gmlwjdchl82@naver.com>
2022-10-16 23:47:47 +0900

    cursor style

--- src/components/pages/landing/Landing.js
+++ src/components/pages/landing/Landing.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { mainStyle } from "../../style/GlobalStyle";
 
 export const Landing = () => {
   return (
     <Wrap>
       <TextWrap>
-        <Title>안녕하세요. 신입 개발자 최희정 입니다.</Title>
+        <Title>안녕</Title>
         <Desc>
           <Link to="/home">포트폴리오 보러가기</Link>
         </Desc>
@@ -14,10 +15,45 @@ export const Landing = () => {
   );
 };
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
 
-const TextWrap = styled.div``;
+const TextWrap = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  text-align: center;
+`;
 
-const Title = styled.div``;
+const Title = styled.div`
+  margin-bottom: 50px;
+  position: relative;
+  display: inline-block;
+  color: black;
+  font-size: 1.5rem;
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    right: -5px;
+    width: 2px;
+    height: 100%;
+    background-color: ${mainStyle.blackColor};
+  }
+`;
 
-const Desc = styled.div``;
+const Desc = styled.div`
+  a {
+    color: ${mainStyle.pointColor};
+  }
+`;
