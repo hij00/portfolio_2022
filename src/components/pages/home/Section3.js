d5648877938e496ac00f05ff9e0f2be6dae8bc2e
hijeong_ <gmlwjdchl82@naver.com>
2022-10-31 23:38:03 +0900

    home sec3

--- src/components/pages/home/Section3.js
+++ src/components/pages/home/Section3.js
@@ -1,3 +1,5 @@
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { Container } from "../../../Container";
 import { textSite } from "../../../Text";
@@ -9,8 +11,46 @@ export const Section3 = () => {
       <Container>
         {textSite.map((a) => (
           <ConWrap>
-            <Con></Con>
-            <TextWrap>{a.title}</TextWrap>
+            <Desc className="site">
+              <h3>{a.title}</h3>
+
+              <h6>{a.date}</h6>
+              <h6>{a.toolImg}</h6>
+
+              <p>{a.desc}</p>
+              <p>{a.end}</p>
+
+              <IconWrap>
+                <Icon>
+                  <a
+                    href={a.siteUrl}
+                    className="page"
+                    // style={{
+                    //   backgroundColor: `${
+                    //     view === `${mainStyle.backColor2}`
+                    //       ? `${mainStyle.pointColor2}`
+                    //       : `${mainStyle.pointColor3}`
+                    //   }`,
+                    // }}
+                  >
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                  <a
+                    href={a.gitUrl}
+                    className="git"
+                    // style={{
+                    //   backgroundColor: `${
+                    //     view === `${mainStyle.backColor2}`
+                    //       ? `${mainStyle.pointColor2}`
+                    //       : `${mainStyle.pointColor3}`
+                    //   }`,
+                    // }}
+                  >
+                    <Git>Git Hub</Git>
+                  </a>
+                </Icon>
+              </IconWrap>
+            </Desc>
           </ConWrap>
         ))}
       </Container>
@@ -21,10 +61,42 @@ export const Section3 = () => {
 const Wrap = styled.section`
   width: 100%;
   background-color: ${mainStyle.backColor};
+  border-top: 1px solid white;
+  padding: 100px 0;
 `;
 
-const ConWrap = styled.div``;
+const ConWrap = styled.div`
+  width: 60%;
+  /* background-color: white; */
+  border: 1px solid ${mainStyle.mainColor};
+  border-radius: 50px;
+  margin-bottom: 50px;
+  padding: 30px 30px;
+  position: relative;
+  &::before {
+    content: "title";
+    width: 200px;
+    height: 40px;
+    background-color: ${mainStyle.mainColor};
+    position: absolute;
+    top: -15px;
+    left: -10px;
+    font-size: 30px;
+    font-weight: 900;
+    text-align: center;
+    line-height: 40px;
+    transform: rotate(-10deg);
+  }
+`;
 
 const Con = styled.div``;
 
 const TextWrap = styled.div``;
+
+const Desc = styled.div``;
+
+const IconWrap = styled.div``;
+
+const Icon = styled.div``;
+
+const Git = styled.div``;
