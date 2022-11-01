5e3da00fc8a4fde1210515b32cd50d92164d1e70
hijeong_ <gmlwjdchl82@naver.com>
2022-11-01 22:18:30 +0900

    home sec3

--- src/components/pages/home/Section3.js
+++ src/components/pages/home/Section3.js
@@ -1,56 +1,70 @@
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Container } from "../../../Container";
-import { textSite } from "../../../Text";
+import { textSite, textToy } from "../../../Text";
 import { mainStyle } from "../../style/GlobalStyle";
 
+// const arr = { textSite };
 export const Section3 = () => {
+  // console.log(arr);
+  // const [title, setTitle] = useState();
+
+  // useEffect(() => {
+  //   setTitle(arr);
+  // }, [arr]);
   return (
     <Wrap>
       <Container>
         {textSite.map((a) => (
-          <ConWrap>
-            <Desc className="site">
-              <h3>{a.title}</h3>
-
-              <h6>{a.date}</h6>
-              <h6>{a.toolImg}</h6>
+          <ConWrap key={a.id}>
+            {console.log(typeof a)}
+            <DescWrap className="site">
+              <Desc>
+                <h6>{a.toolImg}</h6>
+                <h6>{a.date}</h6>
+              </Desc>
 
               <p>{a.desc}</p>
               <p>{a.end}</p>
 
               <IconWrap>
                 <Icon>
-                  <a
-                    href={a.siteUrl}
-                    className="page"
-                    // style={{
-                    //   backgroundColor: `${
-                    //     view === `${mainStyle.backColor2}`
-                    //       ? `${mainStyle.pointColor2}`
-                    //       : `${mainStyle.pointColor3}`
-                    //   }`,
-                    // }}
-                  >
+                  <a href={a.siteUrl} className="page">
                     <FontAwesomeIcon icon={faLink} />
                   </a>
-                  <a
-                    href={a.gitUrl}
-                    className="git"
-                    // style={{
-                    //   backgroundColor: `${
-                    //     view === `${mainStyle.backColor2}`
-                    //       ? `${mainStyle.pointColor2}`
-                    //       : `${mainStyle.pointColor3}`
-                    //   }`,
-                    // }}
-                  >
+                  <a href={a.gitUrl} className="git">
                     <Git>Git Hub</Git>
                   </a>
                 </Icon>
               </IconWrap>
-            </Desc>
+            </DescWrap>
+          </ConWrap>
+        ))}
+        {textToy.map((a) => (
+          <ConWrap key={a.id}>
+            {console.log(typeof a)}
+            <DescWrap className="site">
+              <Desc>
+                <h6>{a.toolImg}</h6>
+                <h6>{a.date}</h6>
+              </Desc>
+
+              <p>{a.desc}</p>
+              <p>{a.end}</p>
+
+              <IconWrap>
+                <Icon>
+                  <a href={a.siteUrl} className="page">
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                  <a href={a.gitUrl} className="git">
+                    <Git>Git Hub</Git>
+                  </a>
+                </Icon>
+              </IconWrap>
+            </DescWrap>
           </ConWrap>
         ))}
       </Container>
@@ -67,19 +81,19 @@ const Wrap = styled.section`
 
 const ConWrap = styled.div`
   width: 60%;
-  /* background-color: white; */
-  border: 1px solid ${mainStyle.mainColor};
+  background-color: ${mainStyle.mainColor};
+  /* border: 1px solid ${mainStyle.mainColor}; */
   border-radius: 50px;
-  margin-bottom: 50px;
+  margin-bottom: 100px;
   padding: 30px 30px;
   position: relative;
   &::before {
-    content: "title";
+    content: ${(props) => props.title};
     width: 200px;
     height: 40px;
-    background-color: ${mainStyle.mainColor};
+    background-color: ${mainStyle.pointColor3};
     position: absolute;
-    top: -15px;
+    top: -35px;
     left: -10px;
     font-size: 30px;
     font-weight: 900;
@@ -93,7 +107,30 @@ const Con = styled.div``;
 
 const TextWrap = styled.div``;
 
-const Desc = styled.div``;
+const DescWrap = styled.div`
+  margin-right: 50px;
+  line-height: 24px;
+  h3 {
+    font-size: 22px;
+    font-weight: 900;
+    margin-bottom: 50px;
+  }
+  h6 {
+    font-size: 18px;
+    margin-bottom: 10px;
+    color: gray;
+  }
+  p {
+    margin-top: 10px;
+    font-size: 18px;
+    font-weight: 300;
+  }
+`;
+
+const Desc = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const IconWrap = styled.div``;
 
