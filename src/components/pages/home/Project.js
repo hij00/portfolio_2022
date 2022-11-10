8fc70554c75aa045af5d28e9b1fb9cd219a27604
hijeong_ <gmlwjdchl82@naver.com>
2022-11-10 23:54:05 +0900

    project style

--- src/components/pages/home/Project.js
+++ src/components/pages/home/Project.js
@@ -23,30 +23,22 @@ export const Project = ({ a, idx }) => {
         <p>{a.desc}</p>
         <p>{a.end}</p>
 
-        <IconWrap>
-          <Icon>
-            <a href={a.siteUrl} className={isEven ? "blue_icon" : "green_icon"}>
-              <FontAwesomeIcon icon={faLink} />
-            </a>
-            <a href={a.gitUrl} className={isEven ? "blue_icon" : "green_icon"}>
-              <Git
-              // style={{
-              //   background: `url(${github}) no-repeat left/cover`,
-              // }}
+        <Icon>
+          <a href={a.siteUrl} className={isEven ? "blue_icon" : "green_icon"}>
+            <FontAwesomeIcon icon={faLink} />
+          </a>
+          <a href={a.gitUrl} className={isEven ? "blue_icon" : "green_icon"}>
+            <Git>
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                {/* {github && <github />} */}
-                <svg
-                  role="img"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  // fill={mainStyle.mainColor}
-                >
-                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                </svg>
-              </Git>
-            </a>
-          </Icon>
-        </IconWrap>
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+              </svg>
+            </Git>
+          </a>
+        </Icon>
       </TextWrap>
     </Con>
   );
@@ -75,13 +67,13 @@ const Con = styled.div`
 
 const Title = styled.div`
   &.blue_title {
-    right: -20px;
-    transform: rotate(10deg);
+    /* position: absolute;
+    right: -20px; */
+    /* transform: rotate(10deg); */
     background-color: ${mainStyle.pointColor3};
     top: -35px;
     width: 200px;
     height: 40px;
-    position: absolute;
 
     font-size: 30px;
     font-weight: 900;
@@ -89,13 +81,13 @@ const Title = styled.div`
     line-height: 40px;
   }
   &.green_title {
-    left: -20px;
-    transform: rotate(-10deg);
+    /* position: absolute;
+    left: -20px; */
+    /* transform: rotate(-10deg); */
     background-color: ${mainStyle.pointColor2};
     top: -35px;
     width: 200px;
     height: 40px;
-    position: absolute;
 
     font-size: 30px;
     font-weight: 900;
@@ -105,7 +97,11 @@ const Title = styled.div`
 `;
 
 const TextWrap = styled.div`
-  margin-right: 50px;
+  /* margin-right: 50px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   line-height: 24px;
   h3 {
     font-size: 22px;
@@ -129,14 +125,11 @@ const Text = styled.div`
   justify-content: space-between;
 `;
 
-const IconWrap = styled.div``;
-
 const Icon = styled.div`
   width: 50px;
   height: 50px;
   display: flex;
-  position: absolute;
-  bottom: -20px;
+  margin-top: 20px;
   a.blue_icon {
     display: flex;
     justify-content: center;
