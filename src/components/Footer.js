8f25641b91c0d027a75e8f7fb352d46861caef8c
hijeong_ <gmlwjdchl82@naver.com>
2022-12-14 14:58:14 +0900

    footer uselocation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { mainStyle } from "./styles/GlobalStyle";
 
 export const Footer = () => {
+  const location = useLocation();
+  const [show, setShow] = useState("flex");
+
+  useEffect(() => {
+    const handleClick = () => {
+      if (location.pathname === "/project") {
+        setShow("none");
+      } else {
+        setShow("flex");
+      }
+    };
+    handleClick();
+  }, [location]);
+
   return (
-    <Wrap>
+    <Wrap show={show}>
       <Text>&copy;Choi Heejeong</Text>
     </Wrap>
   );
@@ -12,7 +28,7 @@ export const Footer = () => {
 const Wrap = styled.div`
   border-top: 1px solid ${mainStyle.blackColor};
   padding: 50px 0;
-  display: flex;
+  display: ${(props) => props.show};
   justify-content: center;
   align-items: center;
 `;
